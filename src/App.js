d641267102dcfe143ceeb53615f45ee5c79db138
Nosov-Pavel <nopaev@gmail.com>
2021-08-18 11:40:22 -0400

    phonN

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Phones from "./Phones";
 
 function App() {
+    const phone = '+1 321 987 69 85 '
   return (
     <div>
       Component App
+
+        <Phones phoneN={phone}/>
 
     </div>
   );
---
+++ src/Phones.js
@@ -0,0 +1,11 @@
+function Phones(props){
+
+    console.log(props)
+    return(
+        <div>
+            Component Phones {props.phoneN}
+
+        </div>
+    );
+}
+export default Phones;
