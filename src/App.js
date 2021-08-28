989c0f656ca3a3cf8d26b7a25cce0148296cfebf
Nosov-Pavel <nopaev@gmail.com>
2021-08-28 13:44:38 -0400

    HelloButton

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
-import Phones from "./Phones";
-
-function App() {
-    const phone = '+1 321 987 69 85'
-    const phone2 = '+1 321 987 69 73'
-
-    const person = {
-        name: 'Viktor',
-        phone: '+1 707 590 18 67',
-        address: {
-            state: 'CA',
-            sity: 'Commerce',
-            zip: '324561-373'
-        }
+function App(){
+    const hello = () => {
+        console.log('Hello')
+        
     }
-  return (
-    <div>
-      Component App
 
-        <Phones
-            phoneN={phone}
-            phoneN2={phone2}
-            person={person}
-        />
-
-    </div>
-  );
+    return(
+        <div>
+            <button onClick={hello}>Hello</button>
+        </div>
+    );
 }
-
 export default App;
